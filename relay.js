816bdd165fc03746bcc1d120ed3f98ecef8b48f4
wwilliamx07 <wwilliamx07@gmail.com>
2025-10-05 02:38:41 -0400

    u

--- relay.js
+++ relay.js
@@ -4,7 +4,7 @@ import { readFileSync } from "fs"
 import { GoogleGenAI } from "@google/genai"
 
 const app = express()
-const port = 443
+const port = 8080
 
 app.use(cors())
 app.use(express.json())
